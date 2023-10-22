bad826c1ccb2d51a87813ec67bbde8457ede1b94
Blaine <Blaineevey@gmail.com>
2023-10-22 14:02:30 -0400

    updated version

--- Kinematic Arrive/Assets/scripts/Kinematic.cs
+++ Kinematic Arrive/Assets/scripts/Kinematic.cs
@@ -7,7 +7,9 @@ public class Kinematic : MonoBehaviour
 
     [SerializeField] private Transform mover;
     [SerializeField] private Rigidbody rigidBodMover;
-    [SerializeField] private Transform target;
+
+    private Vector3 targetLocation;
+    
     [SerializeField] private float speed;
     [SerializeField] private float stop;
 
@@ -15,18 +17,41 @@ public class Kinematic : MonoBehaviour
     void Start()
     {
         speed = 2.0f;
-        stop = 3.0f;
+        stop = 1.0f;
+        targetLocation = mover.position;
     }
 
-    // Update is called once per frame
+    //allows for user input since fixed update is janky
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            targetLocation = clickocation();
+        }
+    }
+    
+    //makes physics run smoother
     void FixedUpdate()
     {
-        
-        Vector3 towards = target.position - mover.position;
+        Vector3 towards = targetLocation - mover.position;
         
         if (towards.magnitude <= stop) { return; }
         mover.rotation = Quaternion.Lerp(mover.rotation, Quaternion.LookRotation(towards), .1f);
         rigidBodMover.velocity = transform.forward.normalized * speed;
 
+    }
+
+    //the Kinematic part of the assinment
+    Vector3 clickocation()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+             return hit.point;
+        }
+        return targetLocation;
+        
     }
 }
