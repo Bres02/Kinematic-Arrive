72d879265641f107cbd22fab220e8fedab108e24
Blaine <Blaineevey@gmail.com>
2023-10-23 17:21:23 -0400

    attempt at movement(i know its wrong)

--- Kinematic Arrive/Assets/scripts/Kinematic.cs
+++ Kinematic Arrive/Assets/scripts/Kinematic.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Kinematic : MonoBehaviour
 {
+    private Ray front, left, right;
+    [SerializeField] private float lineLength;
+
+    [SerializeField] private GameObject wall;
 
     [SerializeField] private Transform mover;
     [SerializeField] private Rigidbody rigidBodMover;
@@ -17,7 +21,7 @@ public class Kinematic : MonoBehaviour
     void Start()
     {
         speed = 2.0f;
-        stop = 1.0f;
+        stop = 3.0f;
         targetLocation = mover.position;
     }
 
@@ -28,17 +32,74 @@ public class Kinematic : MonoBehaviour
         {
             targetLocation = clickocation();
         }
+        if(Input.GetMouseButtonDown(1)) 
+        {
+            Instantiate(wall, new Vector3(clickocation().x, 6.25f, clickocation().z), Quaternion.identity);
+        }
     }
-    
     //makes physics run smoother
     void FixedUpdate()
     {
         Vector3 towards = targetLocation - mover.position;
         
         if (towards.magnitude <= stop) { return; }
-        mover.rotation = Quaternion.Lerp(mover.rotation, Quaternion.LookRotation(towards), .1f);
-        rigidBodMover.velocity = transform.forward.normalized * speed;
 
+
+        var rotation = this.transform.rotation;
+        var rotationModleft = Quaternion.AngleAxis((0 / ((float)2)) * 45 - (45 / 2), this.transform.up);
+        var rotationModfront = Quaternion.AngleAxis((1 / ((float)2)) * 45 - (45 / 2), this.transform.up);
+        var rotationModright = Quaternion.AngleAxis((2 / ((float)2)) * 45 - (45 / 2), this.transform.up);
+
+        var directionleft = rotation * rotationModleft * Vector3.forward;
+        var directionfront = rotation * rotationModfront * Vector3.forward;
+        var directionright = rotation * rotationModright * Vector3.forward;
+
+        right = new Ray(this.transform.position, directionright);
+        front = new Ray(this.transform.position, directionfront);
+        left = new Ray(this.transform.position, directionleft);
+
+
+        RaycastHit hitInfoLeft;
+        RaycastHit hitInfoFront;
+        RaycastHit hitInfoRight;
+
+        if (Physics.Raycast(left, out hitInfoLeft, lineLength))
+        {
+            Debug.DrawRay(transform.position, left.direction * lineLength, Color.red);
+
+        }
+        else if (Physics.Raycast(right, out hitInfoRight, lineLength))
+        {
+            Debug.DrawRay(transform.position, right.direction * lineLength, Color.red);
+
+        }
+        else if (Physics.Raycast(front, out hitInfoFront, lineLength))
+        {
+            
+                Debug.DrawRay(transform.position, front.direction * lineLength, Color.red);
+
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, right.direction * lineLength, Color.green);
+            Debug.DrawRay(transform.position, left.direction * lineLength, Color.green); 
+            Debug.DrawRay(transform.position, front.direction * lineLength, Color.green);
+
+        }
+        if( !Physics.Raycast(front, out hitInfoFront,lineLength))
+        {
+            mover.rotation = Quaternion.Lerp(mover.rotation, Quaternion.LookRotation(towards), .1f);
+            rigidBodMover.velocity = transform.forward.normalized * speed;
+        }
+        else 
+        {
+            if (!Physics.Raycast(left, out hitInfoLeft, lineLength))
+            {
+                mover.rotation = Quaternion.Lerp(mover.rotation, Quaternion.LookRotation(-right.direction), .1f);
+            }
+            rigidBodMover.velocity = transform.forward.normalized * speed;
+
+        }
     }
 
     //the Kinematic part of the assinment
@@ -52,6 +113,5 @@ public class Kinematic : MonoBehaviour
              return hit.point;
         }
         return targetLocation;
-        
     }
 }
